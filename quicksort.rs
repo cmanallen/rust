d4f68efb8cd65757f652fc56c0b4d07986b17fd8
Colton Allen <cmanallen90@gmail.com>
2017-12-27 11:44:48 -0800

    Initial commit

---
+++ quicksort.rs
@@ -0,0 +1,49 @@
+
+
+fn quicksort(array: &mut[isize], first: usize, last: usize) {
+    if first < last {
+        let midpoint = partition(array, first, last);
+        quicksort(array, first, midpoint - 1);
+        quicksort(array, midpoint + 1, last);
+    }
+}
+
+
+fn partition(array: &mut[isize], first: usize, last: usize) -> usize {
+    let pivot = array[last];
+
+    let i: isize = first as isize;
+    let mut i: isize = i - 1;
+
+    let mut j = first;
+    while j < last - 1 {
+        if array[j] < pivot {
+            i = i + 1;
+            let k: usize = i as usize;
+            swap(array, k, j);
+        }
+        j = j + 1;
+    }
+
+    let k: isize = i + 1;
+    let k: usize = k as usize;
+    if array[last] < array[k] {
+        swap(array, k, last);
+    }
+    return k;
+}
+
+
+fn swap(array: &mut[isize], a: usize, b: usize) {
+    let temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+
+fn main() {
+    let mut array = [3, 5, 1, 4, 2];
+    quicksort(&mut array, 0, 4);
+
+    println!("{:?}", array);
+}
